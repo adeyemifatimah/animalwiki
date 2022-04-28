5f541b295ba9f6ef69f03119a348901c2c0c7a62
abisoye <beesoye111@gmail.com>
2022-04-28 20:27:44 +0100

    use of toggle and slide show

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$ (document).ready(function () {
+    $("#de").click(function(){
+        $("#deer").slideToggle(300);
+      });
+
+
+
+
+      $("#ow").click(function(){
+        $("#owl").slideToggle(300);
+      });
+
+
+
+      $("#lep").click(function(){
+        $("#leo").slideToggle(300);
+      });
+
+
+
+
+
+
+
+
+
+
+
+});         
